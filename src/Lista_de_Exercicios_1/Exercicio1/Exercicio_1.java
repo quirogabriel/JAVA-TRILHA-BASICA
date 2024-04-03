5c8c5693a2bb952c335d64201bd91c7922fef47f
Gabriel Alves Quiroga <gb.quiroga20@gmail.com>
2024-04-03 18:20:29 -0300

    testando commit

--- src/Lista_de_Exercicios_1/Exercicio1/Exercicio_1.java
+++ src/Lista_de_Exercicios_1/Exercicio1/Exercicio_1.java
@@ -3,7 +3,7 @@ package Lista_de_Exercicios_1.Exercicio1;
 import java.time.LocalDate;
 import java.time.Month;
 
-public class Exercicio_1 {
+public class ExercicioUm {
     public static void main(String[] args) {
 
         Pessoa pessoa = new Pessoa("Gabriel", LocalDate.of(1997, Month.OCTOBER, 16), 1.80);
